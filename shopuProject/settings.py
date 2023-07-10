444a50a999a5e037f631dd3272449e67d899229c
badr mohammed <badralmutairi111@gmail.com>
2023-07-11 01:25:39 +0300

    final

--- shopuProject/settings.py
+++ shopuProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*^8-5ch5fv#l^wf9(@1%qgcs4k%rf^egbb)s#ckj%+7@81tu-r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
